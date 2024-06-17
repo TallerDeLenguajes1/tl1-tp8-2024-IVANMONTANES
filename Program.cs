106ddfcf4e20a62da5209acb14e53cf1b8ff32bd
ivan montañes <ivanmontanesfilipit@gmail.com>
2024-06-17 18:54:48 -0300

    transferir lista

--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@
 //< >//
 bool seguir = true;
 int id = 1;
+//definimos las listas//
 List<Tarea> tareaPendientes = new List<Tarea>();
+List<Tarea> tareaRealizadas = new List<Tarea>();
 //cargar tareas//
-do{
+do
+{
     String seguirCadena;
     Tarea nuevaTarea = new Tarea();
     nuevaTarea.crearTarea(id);
@@ -12,14 +15,43 @@ do{
     id++;
     Console.WriteLine("1 para añadir una nueva tarea");
     seguirCadena = Console.ReadLine();
-    if(seguirCadena != "1"){
+    if (seguirCadena != "1")
+    {
         seguir = false;
     }
-}while(seguir);
+} while (seguir);
 
 //mostrar tareas//
-foreach (var tarea in tareaPendientes)
-{
-  tarea.mostrarTarea();  
-};
+mostrarLista(tareaPendientes,"tareas pendientes");
+//interfaz para transferir tareas//
+string idElegirC;
+int idElegir,indice = 0,iteracion = 0,tareaEncontrada = 0;
+Console.WriteLine("----------TRANSFERIR TAREAS----------");
+mostrarLista(tareaPendientes,"tareas pendientes");
+Console.WriteLine("indique la tarea por id:");
+idElegirC = Console.ReadLine();
+int.TryParse(idElegirC,out idElegir);
+    foreach(Tarea tarea in tareaPendientes){
+        if(tarea.TareaId == idElegir){
+                indice = iteracion;
+                tareaEncontrada = 1; 
+        }
+        iteracion++; 
+    }
 
+if(tareaEncontrada == 1){
+tareaRealizadas.Add(tareaPendientes[indice]);
+tareaPendientes.RemoveAt(indice);
+}
+mostrarLista(tareaPendientes,"tareas pendientes");
+mostrarLista(tareaRealizadas,"tareas realizadas");
+
+
+static void mostrarLista(List<Tarea> lista,String nombreLista)
+{
+    Console.WriteLine("----------"+nombreLista+"----------");
+    foreach (var tarea in lista)
+    {
+        tarea.mostrarTarea();
+    };
+}
